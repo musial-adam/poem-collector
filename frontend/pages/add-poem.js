38d2b9b0fe137fa9de0e8b6f3deef8bb4453d315
Adam Musiał <adam.musiall@gmail.com>
2019-07-12 14:05:24 +0200

    configure development env for frontend

--- frontend/pages/add-poem.js
+++ frontend/pages/add-poem.js
@@ -1,18 +1,8 @@
-import styled from 'styled-components'
-
-const StyledTest = styled.div`
-  height: 50px;
-  width: 50px;
-  background-color: red;
-  /* color: ; */
-`
-
 function AddPoem() {
   return (
     <div>
-      <h1>test h1 numer 2</h1>
+      <h1>Add Poem page</h1>
       Welcome to Next.js!
-      <StyledTest />
     </div>
   )
 }
